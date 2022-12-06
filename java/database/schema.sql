51ec9b8972886f3e360f303752a1089156804cb7
Matt Scarpino <mattscarpino22@gmail.com>
2022-12-06 09:50:58 -0500

    database work

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,5 +1,7 @@
 BEGIN TRANSACTION;
 
+DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS bands;
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
@@ -8,6 +10,33 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE bands
+(
+	name varchar(100) NOT NULL,
+	description varchar(1000),
+	members varchar(1000) NOT NULL,
+	user_id INTEGER NOT NULL,
+	band_id SERIAL NOT NULL,
+	
+	CONSTRAINT PK_band_id PRIMARY KEY(band_id),
+	CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE followers
+(
+	user_id INTEGER NOT NULL,
+	band_id INTEGER NOT NULL,
+	
+	CONSTRAINT FK_band_id FOREIGN KEY (band_id) REFERENCES bands(band_id),
+	CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(user_id),
+	PRIMARY KEY(band_id, user_id)
+);
+
+CREATE TABLE messages
+(
+	
 );
 
 COMMIT TRANSACTION;
