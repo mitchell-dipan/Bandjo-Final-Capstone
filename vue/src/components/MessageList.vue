d05be0f8324c0fe512f37e23cfaeaafa1559aa01
Mitchell DiPangrazio <dipangraziom@gmail.com>
2022-12-07 12:36:45 -0500

    timeline styling

--- vue/src/components/MessageList.vue
+++ vue/src/components/MessageList.vue
@@ -1,14 +1,15 @@
 <template>
   <main>
-    <img src="../../assets/notes_logo.png" />
-
-    <div id="message-box" v-for="message in messages" :key="message.id">
-      <div class="profile">
-        <img src="../../assets/bandjo-logo-final-removebg-preview.png" />
-        <h1>{{ message.name }}</h1>
+    <div class="timeline-box">
+      <img id="notes-logo" src="../../assets/notes-logo.png" />
+      <div id="message-box" v-for="message in messages" :key="message.id">
+        <div class="top-row">
+          <img src="../../assets/bandjo-logo-final-removebg-preview.png" />
+          <h1>{{ message.name }}</h1>
+          <p>Posted on {{ message.messageDate }}</p>
+        </div>
+        <p id="message">{{ message.messageContent }}</p>
       </div>
-      <h3>{{ message.messageContent }}</h3>
-      <p>Posted on {{ message.messageDate }}</p>
     </div>
   </main>
 </template>
@@ -46,29 +47,53 @@ main {
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
-img {
-  width: 10%;
+.timeline-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 40%;
 }
 #message-box {
   display: flex;
   flex-direction: column;
-  align-items: center;
-  box-shadow: inset 2px 2px 5px #babecc, inset -5px -5px 10px white;
-  width: 50%;
+  border: solid 1px #ef8354;
+  width: 100%;
+  margin-top: 3%;
+  padding-bottom: 5%;
+  padding-top: 2%;
+  background-color: #2d3142;
   border-radius: 10px;
-  margin-top: 2%;
 }
-.profile {
+#message-box:hover {
+  background-color: #3b4057;
+}
+.top-row {
   display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 3%;
 }
-#message-box img {
+.top-row img {
   width: 10%;
   border: 1px solid black;
   border-radius: 100%;
-  margin-top: 2%;
-  margin-left: 5%;
-}
-#message-box h1 {
   margin-left: 2%;
+}
+.top-row h1 {
+  margin-right: 35%;
+  color: white;
+}
+.top-row p {
+  margin-right: 1%;
+  color: white;
+}
+#message {
+  padding-left: 3%;
+  color: white;
+}
+
+#notes-logo {
+  align-self: center;
+  width: 20%;
 }
 </style>
