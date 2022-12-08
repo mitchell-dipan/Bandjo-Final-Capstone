5bde913c8ff503d161278b801efa9db753837733
Matt Scarpino <mattscarpino22@gmail.com>
2022-12-08 13:41:11 -0500

    create band page

--- vue/src/components/CreateBand.vue
+++ vue/src/components/CreateBand.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
     <h1>Create a Band</h1>
-    <input type="text" placeholder="Band Name" v-model="newBand.name" />
+    <input
+      class="type"
+      name="name"
+      type="text"
+      placeholder="Band Name"
+      v-model="newBand.name"
+    />
+
     <input
       type="text"
+      class="type"
       placeholder="Description"
       v-model="newBand.description"
     />
-    <input type="text" placeholder="Members" v-model="newBand.members" />
     <input
+      class="type"
+      type="text"
+      placeholder="Members"
+      v-model="newBand.members"
+    />
+    <input
+      class="type"
       type="text"
       placeholder="Profile pic url"
       v-model="newBand.profilePic"
@@ -34,4 +48,11 @@ export default {
 </script>
 
 <style>
+h1 {
+  justify-content: center;
+}
+
+.type {
+  width: 50%;
+}
 </style>
--- vue/src/views/MakeBand.vue
+++ vue/src/views/MakeBand.vue
@@ -1,17 +1,15 @@
 <template>
-  <div>
-    <nav-bar><create-band></create-band></nav-bar>
-  </div>
+  <div><create-band></create-band></div>
 </template>
 
 <script>
 import CreateBand from "../components/CreateBand.vue";
-import NavBar from "../components/navbar.vue";
+// import NavBar from "../components/navbar.vue";
 export default {
   name: "make-band",
   components: {
+    // NavBar,
     CreateBand,
-    NavBar,
   },
 };
 </script>
