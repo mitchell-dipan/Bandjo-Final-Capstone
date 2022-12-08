5738ea301087dcbcc75d5c631f5e17547b5a4afa
Mitchell DiPangrazio <dipangraziom@gmail.com>
2022-12-08 11:57:55 -0500

    push

--- vue/src/components/BandCard.vue
+++ vue/src/components/BandCard.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+  <div class="card">
+    <img id="prof-pic" src="" />
     <h1>{{ band.name }}</h1>
     <h2>{{ band.members }}</h2>
     <p>{{ band.description }}</p>
@@ -12,7 +13,11 @@
     <ul v-for="genre in genres" v-bind:key="genre.id">
       <li>{{ genre.genreName }}</li>
     </ul>
-    <ul v-for="picture in pictures" v-bind:key="picture.id">
+    <ul
+      v-for="picture in pictures"
+      v-bind:key="picture.id"
+      class="picture-gallery"
+    >
       <li><img v-bind:src="picture.pic_url" /></li>
     </ul>
   </div>
@@ -49,5 +54,40 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.card {
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 20%;
+  width: 70%;
+  border-left: #2d3142 solid 2px;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+#prof-pic {
+  border: solid black 2px;
+  border-radius: 100%;
+}
+.picture-gallery {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+}
+.picture-gallery > li {
+  flex-basis: 350px;
+}
+.picture-gallery li img {
+  object-fit: cover;
+  max-width: 50%;
+  height: auto;
+  vertical-align: middle;
+  border-radius: 5px;
+}
+.picture-gallery::after {
+  content: "";
+  flex-basis: 350px;
+}
 </style>
--- vue/src/views/BandPage.vue
+++ vue/src/views/BandPage.vue
@@ -1,13 +1,15 @@
 <template>
-  <div><band-card></band-card></div>
+  <div><nav-bar></nav-bar><band-card></band-card></div>
 </template>
 
 <script>
 import BandCard from "../components/BandCard.vue";
+import NavBar from "../components/navbar.vue";
 export default {
   name: "band-page",
   components: {
     BandCard,
+    NavBar,
   },
 };
 </script>
