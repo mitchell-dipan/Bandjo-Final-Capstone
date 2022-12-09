cc3b71f0bf0c3ce0884a46b5e8fcec995ce6d4f7
Mitchell DiPangrazio <dipangraziom@gmail.com>
2022-12-09 12:43:29 -0500

    push

--- vue/src/views/MakeBand.vue
+++ vue/src/views/MakeBand.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <!-- <nav-bar></nav-bar> -->
+    <nav-bar></nav-bar>
     <create-band></create-band>
   </div>
 </template>
 
 <script>
 import CreateBand from "../components/CreateBand.vue";
-// import NavBar from "../components/navbar.vue";
+import NavBar from "../components/navbar.vue";
 export default {
   name: "make-band",
   components: {
-    // NavBar,
+    NavBar,
     CreateBand,
   },
 };
 </script>
 
-<style>
+<style scoped>
 </style>
